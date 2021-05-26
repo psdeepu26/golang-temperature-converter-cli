1aa4e243d307af3677c8b52ff3ae3429dbd72f1f
Santhosh Deepu Patrayuni <spatrayuni@vmware.com>
2021-05-26 19:08:02 +0530

    Validate arguments & read origin unit

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"strings"
 )
 
 var originUnit string
@@ -17,8 +18,12 @@ var errInvalidArguments = errors.New("Invalid arguments")
 var errReadingInput = errors.New("Error reading input")
 
 func main() {
-
+	// fmt.Println(len(os.Args))
+	if len(os.Args) != 2 {
+		printError(errInvalidArguments)
+	}
 	for {
+		originUnit = strings.ToUpper(os.Args[1])
 		fmt.Print("What is the current temperature in " + originUnit + " ? ")
 
 		fmt.Print("Would you like to convert another temperature ? (y/n) ")
